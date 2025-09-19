9f71ed463bea47749d983cdbf8b00e8fe2c164c9
Nick <nick71250@gmail.com>
2025-09-19 10:07:05 +0400

    Commit_190925
    Commint for transferring to MacOs

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Hello!!!");
+
+const todoInput = document.querySelector('.todo__input');
+const todoBtn = document.querySelector('.todo__btn');
+const todoList = document.querySelector('.todo__list');
+
+todoBtn.addEventListener('click', function () {
+  addTask()
+})
+
+function addTask() {
+  if (todoInput.value === '') {
+    alert('Нужно ввести текст задачи!');
+  } else {
+    let li = document.createElement('li')
+    li.innerHTML = todoInput.value
+    todoList.appendChild(li)
+    let span = document.createElement('span')
+    span.innerHTML = '\u00d7'
+    li.appendChild(span)
+  }
+  todoInput.value = ''
+}
+
+todoList.addEventListener('click', function (e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked')
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove()
+  }
+})
